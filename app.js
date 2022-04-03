64c7ca280d0c661a7294e416576cc016cae7057c
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-04-03 21:39:19 +1200

    Sunday night madness

--- app.js
+++ app.js
@@ -998,4 +998,88 @@ else {
 testElseIf(7);
 }
 
+Order is important in if, else if statements.
+
+The function is executed from top to bottom so you will want
+to be careful of what statement comes first.
+
+Take these two functions as an example.
+
+Here's the first:
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+And the second just switches the order of the statements:
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+While these two functions look nearly identical if we pass a number to both we get different outputs.
+
+foo(0)
+bar(0)
+foo(0) will return the string Less than one, and bar(0) will return the string Less than two.
+
+Test 73
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+if/else statements can be chained together for complex logic.
+Here is pseudocode of multiple chained if / else if statements:
+
+if (condition1) {
+  statement1
+} else if (condition2) {
+  statement2
+} else if (condition3) {
+  statement3
+. . .
+} else {
+  statementN
+}
+
+Test 74
+
+function testSize(num) {
+  // Only change code below this line
+if (num < 5) {
+  return 'Tiny'
+} else if (num < 10) {
+  return 'Small'
+} else if (num < 15) {
+  return 'Medium'
+} else if (num < 20){
+  return 'Large'
+} else (num < 5);{
+
+
+  return "Huge"};
+  // Only change code above this line
+}
+
+testSize(7);
+
 */
