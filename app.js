d463e2193529f8090a1bbb8cd88580b8d6d4111f
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-04-04 20:44:18 +1200

    More freeCodeCamp

--- app.js
+++ app.js
@@ -1127,4 +1127,29 @@ case values are tested with strict equality (===).
 The break tells JavaScript to stop executing statements.
 If the break is omitted, the next statement will be executed.
 
+Test 76
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case 1:
+  answer = "alpha";
+  break;
+  case 2:
+  answer = "beta";
+  break;
+  case 3:
+  answer = "gamma";
+  break;
+  case 4:
+  answer = "delta"
+}
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
 */
