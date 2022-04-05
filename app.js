77344d709433b8033c6f269af65c41e99735ea05
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-04-05 13:51:54 +1200

    More freeCodeCamp

--- app.js
+++ app.js
@@ -1152,4 +1152,176 @@ switch(val){
 
 caseInSwitch(1);
 
-*/
+In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.
+
+A default statement should be the last case.
+
+switch (num) {
+  case value1:
+    statement1;
+    break;
+  case value2:
+    statement2;
+    break;
+...
+  default:
+    defaultStatement;
+    break;
+}
+
+Test 77
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case "a":
+  answer = "apple";
+  break;
+  case "b":
+  answer = "bird";
+  break;
+  case "c":
+  answer = "cat";
+  break;
+  default:
+  answer = "stuff";
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+If the break statement is omitted from a switch statement's case,
+the following case statement(s) are executed until a break is encountered.
+If you have multiple inputs with the same output,
+you can represent them in a switch statement like this:
+
+let result = "";
+switch(val) {
+  case 1:
+  case 2:
+  case 3:
+    result = "1, 2, or 3";
+    break;
+  case 4:
+    result = "4 alone";
+}
+Cases for 1, 2, and 3 will all produce the same result.
+
+Test 78
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+  answer = "Low";
+  break;
+  case 4:
+  case 5:
+  case 6:
+  answer = "Mid";
+  break;
+  case 7:
+  case 8:
+  case 9:
+  answer = "High"
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+If you have many options to choose from,
+a switch statement can be easier to write than
+many chained if/else if statements.
+
+The following:
+
+if (val === 1) {
+  answer = "a";
+} else if (val === 2) {
+  answer = "b";
+} else {
+  answer = "c";
+}
+can be replaced with:
+
+switch(val) {
+  case 1:
+    answer = "a";
+    break;
+  case 2:
+    answer = "b";
+    break;
+  default:
+    answer = "c";
+}
+Test 79
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch (val){
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42: 
+    answer = "The Answer";
+    break;
+  case 1:
+  answer = "There is no #1";
+  break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+You may recall from Comparison with the Equality Operator that all comparison
+operators return a boolean true or false value.
+
+Sometimes people use an if/else statement to do a comparison, like this:
+
+function isEqual(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+But there's a better way to do this. Since === returns true or false,
+we can return the result of the comparison:
+
+function isEqual(a, b) {
+  return a === b;
+}
+
+Test 80
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a <= b 
+
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
